5f852796125c51e05b1a8e07a09ef45b799858cc
Graham Walters <git@grahamwalters.me>
2017-02-23 19:03:50 -0500

    Users index page (About)

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+export class User {
+  id: number;
+  name: string;
+  username: string;
+  avatar: string;
+}
+
+const users: User[] = [
+  {
+    id: 1,
+    name: 'Chris',
+    username: 'sevilayha',
+    avatar: 'https://pbs.twimg.com/profile_images/826500112549502976/kSENS3xJ_400x400.jpg'
+  },
+  {
+    id: 2,
+    name: 'Nick',
+    username: 'whatnicktweets',
+    avatar: 'https://pbs.twimg.com/profile_images/502500686588690432/wXBzuCBj_400x400.jpeg'
+  },
+  {
+    id: 3,
+    name: 'Holly',
+    username: 'hollylawly',
+    avatar: 'https://pbs.twimg.com/profile_images/721918869821005824/2qT_RY5M_400x400.jpg'
+  }
+];
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+
+  users: User[] = users;
 
   constructor() { }
 
