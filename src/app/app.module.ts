22a734aa9f33bbc76ce2c353737e754e3aea129f
Graham Walters <git@grahamwalters.me>
2017-02-24 00:27:11 -0500

    Lazy load About section of site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AboutModule } from './about/about.module';
 import { appRouting } from './app.routing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -20,7 +19,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AboutModule,
     appRouting
   ],
   providers: [],
